ed8943172a9da0380d4944f40d8cfc35b67f7ec5
ogres <1771626730@qq.com>
2023-06-22 16:36:29 +0800

    迷宫构造

--- src/maze/node.rs
+++ src/maze/node.rs
@@ -1,1 +1,31 @@
+/// This module is maze node
+pub struct Node {
+    column: i16,
+    row: i16,
+    connects: Vec<Box<Node>>,
+    visit: bool,
+    steps: u32,
+}
 
+impl Node {
+    pub fn new(column: i16, row: i16) -> Node {
+        Node {
+            column: column,
+            row: row,
+            connects: Vec<Box<Node>>{},
+            visit: false,
+        }
+    }
+
+    pub fn visit(self) {
+        self.visit = true
+    }
+
+    pub fn hasVisited(self) -> bool {
+        return self.visit;
+    }
+
+    pub fn connects(self, cell: Box<Node>) {
+        self.connects.append(cell);
+    }
+}
